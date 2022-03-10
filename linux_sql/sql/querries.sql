d649379683a48608aeeab5d9b52b03f917864f32
Faizan Ali <m.faizan.ali13@gmail.com>
2022-03-10 19:30:27 +0000

    removed accidentally commited file

--- linux_sql/sql/querries.sql
+++
@@ -1,9 +0,0 @@
---Group hosts by hardware info
---Group hosts by CPU number and sort by their memory size in descending order(within each cpu_number group)
-select cpu_number,id,total_mem
-from host_info
-group by cpu_number,id
-order by cpu_number desc;
-
---Average memory usage
---Average used memory in percentage over 5 mins interval for each host. (used memory = total memory - free memory).
