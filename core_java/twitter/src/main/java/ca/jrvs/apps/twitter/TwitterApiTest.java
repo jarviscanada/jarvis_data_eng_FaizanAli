141bb50a6b93bbe5559c5714c50c29ab5d2a570a
Faizan Ali <m.faizan.ali13@gmail.com>
2022-05-20 17:44:43 -0400

    twitter api testing

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/TwitterApiTest.java
@@ -0,0 +1,42 @@
+package ca.jrvs.apps.twitter;
+
+import com.google.gdata.util.common.base.PercentEscaper;
+import java.util.Arrays;
+import javax.swing.text.html.parser.Entity;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+public class TwitterApiTest {
+  private static String CONSUMER_KEY = System.getenv("consumerKey");
+  private static String CONSUMER_SECRET = System.getenv("consumerSecret");
+  private static String ACCESS_TOKEN = System.getenv("accessToken");
+  private static String TOKEN_SECRET = System.getenv("tokenSecret");
+
+  public static void main(String[] args) throws Exception {
+    //setup oauth
+    OAuthConsumer consumer = new CommonsHttpOAuthConsumer(CONSUMER_KEY,CONSUMER_SECRET);
+    consumer.setTokenWithSecret(ACCESS_TOKEN,TOKEN_SECRET);
+
+    // create an HTTP POST request
+    String status = "today is a good day";
+    PercentEscaper percentEscaper = new PercentEscaper("",false);
+    HttpPost request = new HttpPost(
+        "https://api.twitter.com/1.1/statuses/update.json?status="+ percentEscaper.escape(status));
+
+    //sign the request (add headers)
+    consumer.sign(request);
+
+    System.out.println("Http Request Headers:");
+    Arrays.stream(request.getAllHeaders()).forEach(System.out::println);
+
+    //send the request
+    HttpClient   httpClient = HttpClientBuilder.create().build();
+    HttpResponse response = httpClient.execute(request);
+    System.out.println(EntityUtils.toString(response.getEntity()));
+  }
+}
